a81b1fc6555564a6fc76f86c21017466080511a1
raigordeng <mc.deng@outlook.com>
2025-03-03 09:49:22 +0800

    refactor: add constant for login page

--- src/projects/constants.py
+++ src/projects/constants.py
@@ -1,1 +1,9 @@
 """Module specific constants and error codes."""
+
+from selenium.webdriver.common.by import By
+
+
+class LoginPageLocators:
+    USERNAME_INPUT = (By.ID, "normal_login_username")
+    PASSWORD_INPUT = (By.ID, "normal_login_password")
+    LOG_IN_BUTTON = (By.CSS_SELECTOR, "button.login-form-button")
