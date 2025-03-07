a46a24c4b9c566a9b271c69b62de2f66b0726333
raigordeng <mc.deng@outlook.com>
2025-03-07 09:14:57 +0800

    refactor: path incorrect

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,5 @@ from dotenv import load_dotenv
 
 def load_env():
     """Load environment variables from the .env file in the project root directory."""
-    env_path = os.path.join(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ".env"
-    )
+    env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
     load_dotenv(dotenv_path=env_path)
