204a1d7948b93a5bb4072f0abadbd0cde77677ce
raigordeng <mc.deng@outlook.com>
2025-03-10 09:32:20 +0800

    refactor: rename fields

--- src/projects/constants.py
+++ src/projects/constants.py
@@ -16,10 +16,8 @@ class MainPageLocators:
 class VesyncService:
     """VeSync service constants."""
 
-    OPS_URL: str = "https://ops.vesync.co"
-    PM_URL: str = (
-        "https://pmapi.vesync.co/platform/admin/pmProject/v2/pageProjectSummaryV2"
-    )
+    PM_ORIGIN: str = "https://ops.vesync.co"
+    API_PATH: str = "/platform/admin/pmProject/v2/pageProjectSummaryV2"
     API_CONTEXT: dict = {
         "osInfo": "MacIntel",
         "clientInfo": "pc",
