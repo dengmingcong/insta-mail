771e0b8b12286ddc23db943e0d839b15617e9368
raigordeng <mc.deng@outlook.com>
2025-03-14 09:30:19 +0800

    feat: add api read_project

--- src/projects/router.py
+++ src/projects/router.py
@@ -7,7 +7,8 @@ from fastapi import APIRouter
 
 from src.projects import constants as project_constants
 from src.projects import service as project_service
-from src.projects.schemas import PMProjectLocator
+from src.projects.schemas import PMProject, PMProjectLocator
+from src.projects.utils import get_role_members
 
 router = APIRouter(
     tags=["projects"],
@@ -32,6 +33,7 @@ async def read_projects(
         json={
             "context": {
                 **project_constants.VesyncService.API_CONTEXT,
+                "method": "pageProjectSummaryV2",
                 "accountID": cookies["account_id"],
                 "token": cookies["token"],
                 "traceId": int(datetime.datetime.now().timestamp()),
@@ -52,3 +54,36 @@ async def read_projects(
         PMProjectLocator(id=project["projectId"], title=project["projectFullName"])
         for project in response.json()["result"]["projectList"]
     ]
+
+
+@router.get("/projects/{project_id}")
+async def read_project(project_id: int) -> PMProject:
+    """Query project details by project ID.
+
+    :param project_id: Project ID.
+    """
+    cookies: dict = project_service.auth_pm()
+
+    response = requests.post(
+        project_constants.VesyncService.PM_API_ORIGIN
+        + project_constants.APIPath.GET_PROJECT_MEMBERS,
+        json={
+            "context": {
+                **project_constants.VesyncService.API_CONTEXT,
+                "method": "getRelatedProjectMember",
+                "accountID": cookies["account_id"],
+                "token": cookies["token"],
+                "traceId": int(datetime.datetime.now().timestamp()),
+            },
+            "data": {"projectId": project_id},
+        },
+    )
+
+    raw_members = response.json()["result"]["postMemberList"]
+
+    return PMProject(
+        project_managers=get_role_members(raw_members, "项目经理"),
+        cloud_developers=get_role_members(raw_members, "云开发"),
+        web_developers=get_role_members(raw_members, "web前端开发"),
+        app_developers=get_role_members(raw_members, "app开发"),
+    )
