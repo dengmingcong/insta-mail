aedb7766cdfb92f501d587055b215ff4b047df9d
raigordeng <mc.deng@outlook.com>
2025-03-14 09:29:58 +0800

    chore: add function get_role_members to filter members by role

--- src/projects/utils.py
+++ src/projects/utils.py
@@ -1,1 +1,17 @@
 """Non-business logic functions, e.g. response normalization, data enrichment."""
+
+import jmespath
+
+
+def get_role_members(all_members: list[dict], role: str) -> list[str]:
+    """Get members by role.
+
+    :param all_members: All members of all roles.
+    :param role: Role to filter.
+    :return: Members of the role.
+    """
+    members = jmespath.search(
+        f"[?post.postName=='{role}'] | [0].memberList", all_members
+    )
+
+    return [member["userName"] for member in members]
