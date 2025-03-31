0e88a00680de5c7377418db45f85d261f9044464
raigordeng <mc.deng@outlook.com>
2025-03-31 09:59:37 +0800

    chore: include mail router in main

--- src/main.py
+++ src/main.py
@@ -2,8 +2,17 @@
 
 from fastapi import FastAPI
 
+from src.database import create_db_and_tables
+from src.mails.router import router as mail_router
 from src.projects.router import router as project_router
 
 app = FastAPI()
 
 app.include_router(project_router)
+app.include_router(mail_router)
+
+
+@app.on_event("startup")
+def on_startup():
+    """Create database and tables on startup."""
+    create_db_and_tables()
