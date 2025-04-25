5e1ccddf76951972a5368f9434f9709046408ad4
raigordeng <mc.deng@outlook.com>
2025-04-25 08:40:28 +0800

    chore: add model TokenCreate

--- src/tokens/models.py
+++ src/tokens/models.py
@@ -3,13 +3,18 @@ from datetime import datetime
 from sqlmodel import Field, SQLModel
 
 
-class Token(SQLModel, table=True):
+class TokenCreate(SQLModel):
+    """The data model to create a token."""
+
+    user_id: int  # Foreign key to associate with a user.
+    access_token: str
+    refresh_token: str
+    expires_at: int  # Timestamp in seconds.
+
+
+class Token(TokenCreate, table=True):
     """Model for storing access and refresh tokens."""
 
     id: int | None = Field(default=None, primary_key=True)
-    user_id: int  # Foreign key to associate with a user
-    access_token: str
-    refresh_token: str
-    expires_at: datetime  # Expiration time for the access token
     created_at: datetime | None = Field(default_factory=datetime.now)
     last_updated: datetime | None = Field(default_factory=datetime.now)
