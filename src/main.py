680f082535cd06a43757f464f7639eef29340d35
raigor.deng <raigor.deng@vesync.com>
2025-07-31 09:07:47 +0800

    refactor: rename alias

--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.adapters.router import router as adapters_router
+from src.adapters.router import router as adapter_router
 from src.database import create_db_and_tables
 from src.mails.router import router as mail_router
-from src.tokens.router import router as tokens_router
+from src.users.router import router as user_router
 
 app = FastAPI()
 
-app.include_router(adapters_router)
+app.include_router(adapter_router)
 app.include_router(mail_router)
-app.include_router(tokens_router)
+app.include_router(user_router)
 
 origins = [
     "http://localhost:3000",
