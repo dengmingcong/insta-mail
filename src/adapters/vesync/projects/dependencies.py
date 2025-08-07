6a52729acb2e836930917b0e8910b92fb71704a2
raigordeng <mc.deng@outlook.com>
2025-08-07 08:39:19 +0800

    chore: add dependency to getting fresh user

--- src/adapters/vesync/projects/dependencies.py
+++ src/adapters/vesync/projects/dependencies.py
@@ -1,1 +1,25 @@
 """Router dependencies."""
+
+import datetime
+
+from sqlmodel import select
+
+from src.adapters.vesync.projects.exceptions import NoFreshUserError
+from src.adapters.vesync.projects.models import PmUser
+from src.database import SessionDep
+
+
+def get_fresh_user(session: SessionDep) -> PmUser:
+    """Get the user whose token is not expired and has the latest expires_at."""
+    statement = (
+        select(PmUser)
+        .where(PmUser.expires_at > datetime.datetime.now().timestamp())
+        .order_by(PmUser.expires_at.desc())
+    )
+
+    user = session.exec(statement).first()
+
+    if not user:
+        raise NoFreshUserError()
+
+    return user
