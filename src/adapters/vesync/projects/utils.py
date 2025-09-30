260e9f6522d1a45b3840cd40c699e33f1855fbcc
raigordeng <mc.deng@outlook.com>
2025-09-30 09:59:09 +0800

    docs: add example

--- src/adapters/vesync/projects/utils.py
+++ src/adapters/vesync/projects/utils.py
@@ -6,6 +6,24 @@ import jmespath
 def get_role_members(all_members: list[dict], role: str) -> list[str]:
     """Get members by role.
 
+    Example of one item in ``all_members``::
+
+        {
+            "post": {
+                "postId": 3,
+                "postOrder": 999,
+                "postName": "云测试"
+            },
+            "memberList": [
+                {
+                    "userId": "a009e7ee-b148-103a-8f95-0f7eff6a78aa",
+                    "userName": "raigor.deng",
+                    "userCnName": "邓明聪",
+                    "responsible": true
+                }
+            ]
+        }
+
     :param all_members: All members of all roles.
     :param role: Role to filter.
     :return: Members of the role.
