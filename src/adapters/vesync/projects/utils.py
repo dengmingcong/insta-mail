3acda254fed5f3f43c8e6c3db948e3af75a94307
raigordeng <mc.deng@outlook.com>
2025-10-20 09:39:04 +0800

    chore: add helper get_organization_position_members

--- src/adapters/vesync/projects/utils.py
+++ src/adapters/vesync/projects/utils.py
@@ -1,5 +1,7 @@
 """Non-business logic functions, e.g. response normalization, data enrichment."""
 
+from typing import Union
+
 import jmespath
 
 
@@ -35,3 +37,37 @@ def get_project_position_members(
     )
 
     return [member["userName"] for member in members] if members else []
+
+
+def get_organization_position_members(
+    organization_all_members: list[dict], position: str, is_username_only: bool = True
+) -> list[Union[str, dict]]:
+    """Get members by position in the organization.
+
+    Example of one item in ``organization_all_members``::
+
+        {
+            "userName": "raigor.deng",
+            "itUserId": "a009e7ee-b148-103a-8f95-0f7eff6a78aa",
+            "leader": false,
+            "organizationId": 48,
+            "organizationName": "云平台测试组",
+            "postId": 3,
+            "postName": "云测试",
+            "accountStatus": 1,
+            "loginEnabled": 1,
+            "weeklyReportEnabled": 1
+        }
+
+    :param organization_all_members: All members in the organization.
+    :param position: Position to filter.
+    :param is_username_only: Whether to return only usernames.
+    :return: Members of the position.
+    """
+    expression = (
+        f"[?postName == '{position}'].userName"
+        if is_username_only
+        else f"[?postName == '{position}']"
+    )
+
+    return jmespath.search(expression, organization_all_members)
